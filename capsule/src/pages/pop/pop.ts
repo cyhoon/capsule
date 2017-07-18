8c7b474b16df852ef8c6e7a5e535d447fbe7927d
iHello <ihello0720@gmail.com>
2017-07-18 15:41:09 +0900

    git up!

--- capsule/src/pages/pop/pop.ts
+++ capsule/src/pages/pop/pop.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 /**
  * Generated class for the PopPage page.
@@ -14,10 +15,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PopPage {
 
-  capsule_pk: number;
+  responseData: any; // 응답 데이터
+  requestPk = { "capsule_pk": 0 }; // 서버에 요청할 캡슐의 기본키
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.capsule_pk = navParams.data; // 파라미터 데이터는 캡슐에 pk 데이터임
+  capsuleData = { // 응답받은 캡슐 기본정보
+    "capsule_name" : "",
+    "capsule_date" : "",
+    "capsule_expire" : "",
+    "capsule_latitude" : "",
+    "capsule_longitude" : "",
+    "capsule_media" : ""
+  };
+
+  capsuleFriends = [ // 요청받은 친구들 배열
+
+  ];
+
+  
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public authServiceProvider: AuthServiceProvider) {
+
+    this.requestPk.capsule_pk = navParams.data; // 파라미터 데이터는 캡슐에 pk 데이터임
+
+
   }
 
   ionViewDidLoad() {
