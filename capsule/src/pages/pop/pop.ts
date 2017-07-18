1839e7937e41cce827f35593c2559701974eeee1
iHello <ihello0720@gmail.com>
2017-07-18 17:52:23 +0900

    Hello!

--- capsule/src/pages/pop/pop.ts
+++ capsule/src/pages/pop/pop.ts
@@ -17,6 +17,12 @@ export class PopPage {
 
   responseData: any; // 응답 데이터
   requestPk = { "capsule_pk": 0 }; // 서버에 요청할 캡슐의 기본키
+  friendList: any; // 요청받은 친구 배열
+
+  userDetails = {
+    "user_id" : "",
+    "user_name" : ""
+  };
 
   capsuleData = { // 응답받은 캡슐 기본정보
     "capsule_name" : "",
@@ -27,9 +33,9 @@ export class PopPage {
     "capsule_media" : ""
   };
 
-  capsuleFriends = [ // 요청받은 친구들 배열
-
-  ];
+  capsuleMedia = {
+    "media_path" : ""
+  };
 
   
   
@@ -37,13 +43,87 @@ export class PopPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public authServiceProvider: AuthServiceProvider) {
 
-    this.requestPk.capsule_pk = navParams.data; // 파라미터 데이터는 캡슐에 pk 데이터임
+    const data = JSON.parse(localStorage.getItem('userData'));
+    this.userDetails.user_id = data['user_id'];
+    this.userDetails.user_name = data['user_name'];
+    
+    this.friendList = [
 
+    ];
+
+    this.requestPk.capsule_pk = navParams.data; // 파라미터 데이터는 캡슐에 pk 데이터임
+    this.capsuleLoad();
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PopPage');
+  }
+
+  capsuleLoad () {
+
+    this.authServiceProvider.postData(this.requestPk, '/capsulePop.php').then((result) => {
+      
+      this.responseData = result;
+
+      console.log("캡슐 정보 : " + this.responseData);
+
+      /**
+       *  
+       *  capsuleDate
+          :
+          "2017-07-17 19:54:24"
+          capsuleExpire
+          :
+          "2017-07-18 11:20:00"
+          capsuleName
+          :
+          "최영훈님에 추억"
+          capsule_latitude
+          :
+          "35.6624164"
+          capsule_longitude
+          :
+          "128.4135811"
+
+          mediaPath
+          :
+          [{media0: "uploads/2017071712070724241.jpeg"}]
+          0
+          :
+          {media0: "uploads/2017071712070724241.jpeg"}
+          media0
+          :
+          "uploads/2017071712070724241.jpeg"
+       * 
+       */
+      var capsule_name = this.responseData[0]['capsuleName'];
+      var capsuleDate = this.responseData[0]['capsuleDate'];
+      var capsuleExpire = this.responseData[0]['capsuleExpire'];
+      var capsule_latitude = this.responseData[0]['capsule_latitude'];
+      var capsule_longitude = this.responseData[0]['capsule_longitude'];
+      var media_path = this.responseData[0]['mediaPath'][0]['media0'];
+      var friends = this.responseData[0]['friends'];
+      
+
+      for(var i=0; i<friends.length; i++) {
+        var temp = [{name: friends[i]['friend'+i]}];
+        this.friendList.push(temp);
+      }
+
+      this.capsuleData.capsule_name = capsule_name;
+      this.capsuleData.capsule_date = capsuleDate;
+      this.capsuleData.capsule_expire = capsuleExpire;
+      this.capsuleData.capsule_latitude = capsule_latitude;
+      this.capsuleData.capsule_longitude = capsule_longitude;
+
+      this.capsuleMedia.media_path = media_path;
+
+      console.log("capsuleMedia data : " + this.capsuleMedia.media_path);
+
+
+    });
+
   }
 
 }
